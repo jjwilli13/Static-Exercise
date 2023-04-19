19dea831b76c0fbf698b6bc9a3e8c2466465da53
Jordan Williams <jjwilli13@gmail.com>
2023-04-19 05:47:56 -0500

    completed exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace StaticExercise
+﻿    namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            var fahrenheit = TempConverter.FahrenheitToCelsius(89);
+
+            Console.WriteLine($"The temp after conversion is {fahrenheit} degrees in celsius");
+
+
+
+
+            var celsius = TempConverter.CelsiusToFahrenheit(22);
+
+            Console.WriteLine($"The temp after conversion is {celsius} degrees in fahrenheit");
+
+
+
         }
+
     }
+
+
+
+
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+        }
+
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+
+            var result = fahrenheit - 32 / 1.8;
+
+            return result;
+
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+
+        {
+
+            var result = celsius * 1.8 + 32;
+
+            return result;
+
+        }
+
+
+
+
+    }
+}
+
